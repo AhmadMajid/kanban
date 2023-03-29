109395ab195611d56188e3948c9bc4c4f386242b
Ahmad Majid <Ahmadmajid1994@gmail.com>
2023-03-29 21:57:13 +0100

    forgot to save

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,55 +1,27 @@
-class BoardsController < ApplicationController
+class ListsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @board = Board.new
-  end
-
-  def edit
-    authorize board
-  end
-
-  def show
-    authorize board
+    @list = board.lists.new
   end
 
   def create
-    @board = Board.new(board_params.merge(user: current_user))
+    @list = board.lists.new(list_params)
 
-    if @board.save
-      redirect_to root_path
+    if @list.save
+      redirect_to board_path(board)
     else
       render :new
     end
   end
 
-  def update
-    authorize board
-
-    if board.update(board_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    authorize board
-    board.destroy
-    redirect_to root_path
-  end
-
   private
 
-  def board_params
-    params.require(:board).permit(:name)
-  end
-
   def board
-    @board ||= Board.find(params[:id])
+    @board ||= Board.find(params[:board_id])
   end
 
   def list_params
-    params.require(:lists).permit(:name)
+    params.require(:list).permit(:title)
   end
-end
+ends
